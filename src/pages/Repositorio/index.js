83765ba5d1fdd16d8b306fe2e709087a67a849ae
trinddade29@gmail.com <trinddade29@gmail.com>
2022-01-14 20:18:02 -0300

    criando Owner e Loading style component

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../../services/api";
-import { Container } from "./styles";
+import { Container, Owner, Loading } from "./styles";
 
 export default function Repositorio({ match }) {
   const [repositorio, setRepositorio] = useState({});
@@ -27,9 +27,26 @@ export default function Repositorio({ match }) {
     load();
   }, [match.params.repositorio]);
 
+  if(loading) {
+    return (
+      <Loading>
+        Carregando...
+      </Loading>
+    )
+  }
+
   return (
     <div>
-      <Container style={{ color: "#fff" }}>{repositorio.data}</Container>
+      <Container>
+        <Owner>
+          <img
+            src={repositorio.owner.avatar_url}
+            alt={repositorio.owner.login}
+          />
+          <h1>{repositorio.name}</h1>
+          <p>{repositorio.description}</p>
+        </Owner>
+      </Container>
     </div>
   );
 }
