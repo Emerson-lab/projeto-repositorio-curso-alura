e87eb85f14dd74344922ad85bfdc34c6e330e418
trinddade29@gmail.com <trinddade29@gmail.com>
2022-01-14 20:18:31 -0300

    Arquivo style Woner e Loaging

--- src/pages/Repositorio/styles.js
+++ src/pages/Repositorio/styles.js
@@ -1,3 +1,31 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Loading = styled.div`
+    display: flex;
+    color: #FFFFFF;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+`;
+
+export const Container = styled.div`
+    max-width: 700px;
+    background: #fff;
+    border-radius: 4px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+    padding: 0 30px;
+    margin: 80px auto;
+`;
+
+export const Owner = styled.header`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    img {
+        width: 150px;
+        border-radius: 20%;
+        margin: 20px 0;
+    }
+`;
+
