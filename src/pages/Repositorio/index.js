a5c219090a7a45f362e5977377691d265b2d9e4b
trinddade29@gmail.com <trinddade29@gmail.com>
2022-01-14 23:49:56 -0300

    criando filtre dos repos

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,4 +1,12 @@
-import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+  FilterList,
+} from "./styles";
 import React, { useState, useEffect } from "react";
 import api from "../../services/api";
 import { FaArrowLeft } from "react-icons/fa";
@@ -7,6 +15,13 @@ export default function Repositorio({ match }) {
   const [repositorio, setRepositorio] = useState({});
   const [issues, setIssues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [filters, setFilters] = useState([
+    { state: "all", label: "Todas", action: false },
+    { state: "open", label: "Fechads", action: false },
+    { state: "closed", label: "Abertas", action: true },
+  ]);
+  const [filterIndex, setFilterIndex] = useState(0);
 
   useEffect(() => {
     async function load() {
@@ -16,18 +31,41 @@ export default function Repositorio({ match }) {
         api.get(`/repos/${nomeRepo}`),
         api.get(`/repos/${nomeRepo}/issues`, {
           params: {
-            state: "open",
+            state: filters.find((f) => f.action).state,
             per_page: 5,
           },
         }),
       ]);
       setRepositorio(repositorioData.data);
       setIssues(issuesData.data);
-      console.log(issuesData.data);
       setLoading(false);
     }
     load();
   }, [match.params.repositorio]);
+
+  useEffect(() => {
+    async function loadIssue() {
+      const nomeRepo = decodeURIComponent(match.params.repositorio);
+
+      const response = await api.get(`/repos/${nomeRepo}/issues`, {
+        params: {
+          state: filters[filterIndex].state,
+          page,
+          per_page: 5,
+        },
+      });
+      setIssues(response.data);
+    }
+    loadIssue();
+  }, [filterIndex, match.params.repositorio, page]);
+
+  function handlePage(action) {
+    setPage(action === "back" ? page - 1 : page + 1);
+  }
+
+  function handleFilter(index) {
+    setFilterIndex(index);
+  }
 
   if (loading) {
     return <Loading>Carregando...</Loading>;
@@ -47,6 +85,17 @@ export default function Repositorio({ match }) {
           <h1>{repositorio.name}</h1>
           <p>{repositorio.description}</p>
         </Owner>
+        <FilterList active={filterIndex}>
+          {filters.map((filter, index) => (
+            <button
+              key={filter.label}
+              type="button"
+              onClick={() => handleFilter(index)}
+            >
+              {filter.label}
+            </button>
+          ))}
+        </FilterList>
         <IssuesList>
           <ul>
             {issues.map((item) => (
@@ -54,9 +103,7 @@ export default function Repositorio({ match }) {
                 <img src={item.user.avatar_url} alt={item.user.login} />
                 <div>
                   <strong>
-                    <a href={item.html_url} >
-                      {item.title}
-                    </a>
+                    <a href={item.html_url}>{item.title}</a>
                     {item.labels.map((label) => (
                       <span key={String(label.id)}>{label.name}</span>
                     ))}
@@ -67,6 +114,21 @@ export default function Repositorio({ match }) {
             ))}
           </ul>
         </IssuesList>
+        <PageActions>
+          <>
+            <button
+              type="button"
+              disabled={page === 1 ? true : false}
+              onClick={() => handlePage("back")}
+            >
+              Voltar
+            </button>
+            <p>Página {page}</p>
+            <button type="button" onClick={() => handlePage("next")}>
+              Próxima
+            </button>
+          </>
+        </PageActions>
       </Container>
     </div>
   );
